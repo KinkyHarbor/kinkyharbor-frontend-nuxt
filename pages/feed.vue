fba38d4a275b288854f9fe2af17dc72d2e08a71f
KinkyRobbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-07 22:30:37 +0100

    Add mock feed

--- pages/feed.vue
+++ pages/feed.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <h1>Feed</h1>
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-timeline dense>
+          <v-timeline-item v-for="n in 4" :key="n" large>
+            <template v-slot:icon>
+              <v-avatar>
+                <img src="http://i.pravatar.cc/64" />
+              </v-avatar>
+            </template>
+            <v-card class="elevation-2">
+              <v-card-title class="headline">Lorem ipsum</v-card-title>
+              <v-card-text
+                >Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
+                scaevola imperdiet nec ut, sed euismod convenire principes at.
+                Est et nobis iisque percipit, an vim zril disputando
+                voluptatibus, vix an salutandi sententiae.</v-card-text
+              >
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
