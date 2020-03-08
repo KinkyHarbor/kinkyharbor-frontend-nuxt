198b8c7d268352f4c4ff7b40d823d4d05139d48b
KinkyRobbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-08 20:44:59 +0100

    Fix page titles

--- pages/profile/_user.vue
+++ pages/profile/_user.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1>View user</h1>
+  <h1>View user {{ user_name }}</h1>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      user_name: this.$route.params.user,
+    }
+  },
+
+  head() {
+    return { title: this.user_name }
+  },
+}
+</script>
--- pages/profile/edit.vue
+++ pages/profile/edit.vue
@@ -2,4 +2,14 @@
   <h1>Edit profile</h1>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  head() {
+    return { title: 'Edit profile' }
+  },
+}
+</script>
