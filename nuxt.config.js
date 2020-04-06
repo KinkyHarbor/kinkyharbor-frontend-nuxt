d8475cd6fe3aac34c20c96647695827c209c58c0
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-04-06 20:48:03 +0200

    Disable Refresh schema Will be reactivate once Auth v5 is released

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,248 +1,245 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    titleTemplate: '%s - Harbor',
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content:
-          'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-      {
-        name: 'manifest',
-        href: '/icon/manifest.json',
-      },
-      {
-        name: 'msapplication-TileColor',
-        content: '#ffffff',
-      },
-      {
-        name: 'msapplication-TileImage',
-        content: '/icon/ms-icon-144x144.png',
-      },
-      {
-        name: 'theme-color',
-        content: '#ffffff',
-      },
-    ],
-    link: [
-      {
-        rel: 'apple-touch-icon',
-        sizes: '57x57',
-        href: '/icon/apple-icon-57x57.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '60x60',
-        href: '/icon/apple-icon-60x60.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '72x72',
-        href: '/icon/apple-icon-72x72.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '76x76',
-        href: '/icon/apple-icon-76x76.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '114x114',
-        href: '/icon/apple-icon-114x114.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '120x120',
-        href: '/icon/apple-icon-120x120.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '144x144',
-        href: '/icon/apple-icon-144x144.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '152x152',
-        href: '/icon/apple-icon-152x152.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/icon/apple-icon-180x180.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '192x192',
-        href: '/icon/android-icon-192x192.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/icon/favicon-32x32.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '96x96',
-        href: '/icon/favicon-96x96.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/icon/favicon-16x16.png',
-      },
-    ],
-  },
-  router: {
-    middleware: ['auth'],
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify',
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-    [
-      'nuxt-i18n',
-      {
-        locales: [
-          {
-            code: 'en',
-            iso: 'en-US',
-            name: 'English',
-            file: 'en-US.js',
-          },
-          {
-            code: 'nl',
-            iso: 'nl-BE',
-            name: 'Nederlands',
-            file: 'nl-BE.js',
-          },
-        ],
-        defaultLocale: 'en',
-        fallbackLocale: 'en',
-        lazy: true,
-        langDir: 'lang/',
-        baseUrl: process.env.KH_BASE_URL || 'https://kinkyharbor.com',
-      },
-    ],
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {
-    baseURL: 'http://127.0.0.1:8000',
-  },
-  /*
-   ** Auth module configuration
-   */
-  auth: {
-    strategies: {
-      local: {
-        _scheme: 'refresh',
-        token: {
-          property: 'access_token',
-          maxAge: 60 * 15,
-        },
-        expiresAt: 'exp',
-        refreshToken: {
-          property: 'refresh_token',
-          maxAge: 60 * 60 * 24 * 3,
-        },
-        user: false,
-        endpoints: {
-          login: {
-            url: '/auth/login/',
-            method: 'post',
-            propertyName: 'access_token',
-          },
-          refresh: { url: '/auth/refresh', method: 'post' },
-          logout: false,
-          user: { url: '/users/me/', method: 'get', propertyName: false },
-        },
-        autoLogout: true,
-      },
-    },
-    redirect: {
-      home: '/feed',
-    },
-    plugins: ['~/plugins/auth_redirect-to-locale.js'],
-  },
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.red.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.purple.darken4,
-          warning: colors.amber.darken4,
-          error: colors.deepOrange.accent4,
-          success: colors.lightGreen.base,
-        },
-      },
-    },
-  },
-  /*
-   ** ENV
-   */
-  env: {
-    demoMode: Boolean(process.env.DEMO),
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: 'universal',
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {},
-  },
+    head: {
+        titleTemplate: '%s - Harbor',
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || '',
+            },
+            {
+                name: 'manifest',
+                href: '/icon/manifest.json',
+            },
+            {
+                name: 'msapplication-TileColor',
+                content: '#ffffff',
+            },
+            {
+                name: 'msapplication-TileImage',
+                content: '/icon/ms-icon-144x144.png',
+            },
+            {
+                name: 'theme-color',
+                content: '#ffffff',
+            },
+        ],
+        link: [{
+                rel: 'apple-touch-icon',
+                sizes: '57x57',
+                href: '/icon/apple-icon-57x57.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '60x60',
+                href: '/icon/apple-icon-60x60.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '72x72',
+                href: '/icon/apple-icon-72x72.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '76x76',
+                href: '/icon/apple-icon-76x76.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '114x114',
+                href: '/icon/apple-icon-114x114.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '120x120',
+                href: '/icon/apple-icon-120x120.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '144x144',
+                href: '/icon/apple-icon-144x144.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '152x152',
+                href: '/icon/apple-icon-152x152.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '180x180',
+                href: '/icon/apple-icon-180x180.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '192x192',
+                href: '/icon/android-icon-192x192.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '32x32',
+                href: '/icon/favicon-32x32.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '96x96',
+                href: '/icon/favicon-96x96.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '16x16',
+                href: '/icon/favicon-16x16.png',
+            },
+        ],
+    },
+    router: {
+        middleware: ['auth'],
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/vuetify',
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/auth',
+        '@nuxtjs/pwa',
+        // Doc: https://github.com/nuxt-community/dotenv-module
+        '@nuxtjs/dotenv', [
+            'nuxt-i18n',
+            {
+                locales: [{
+                        code: 'en',
+                        iso: 'en-US',
+                        name: 'English',
+                        file: 'en-US.js',
+                    },
+                    {
+                        code: 'nl',
+                        iso: 'nl-BE',
+                        name: 'Nederlands',
+                        file: 'nl-BE.js',
+                    },
+                ],
+                defaultLocale: 'en',
+                fallbackLocale: 'en',
+                lazy: true,
+                langDir: 'lang/',
+                baseUrl: process.env.KH_BASE_URL || 'https://kinkyharbor.com',
+            },
+        ],
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        baseURL: 'http://127.0.0.1:8000',
+    },
+    /*
+     ** Auth module configuration
+     */
+    auth: {
+        strategies: {
+            local: {
+                // FIXME: Change to refresh once v5 released
+                // _scheme: 'refresh',
+                token: {
+                    property: 'access_token',
+                    maxAge: 60 * 15,
+                },
+                expiresAt: 'exp',
+                refreshToken: {
+                    property: 'refresh_token',
+                    maxAge: 60 * 60 * 24 * 3,
+                },
+                user: false,
+                endpoints: {
+                    login: {
+                        url: '/auth/login/',
+                        method: 'post',
+                        propertyName: 'access_token',
+                    },
+                    refresh: { url: '/auth/refresh', method: 'post' },
+                    logout: false,
+                    user: { url: '/users/me/', method: 'get', propertyName: false },
+                },
+                autoLogout: true,
+            },
+        },
+        redirect: {
+            home: '/feed',
+        },
+        plugins: ['~/plugins/auth_redirect-to-locale.js'],
+    },
+    /*
+     ** vuetify module configuration
+     ** https://github.com/nuxt-community/vuetify-module
+     */
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
+        theme: {
+            dark: true,
+            themes: {
+                dark: {
+                    primary: colors.red.darken2,
+                    accent: colors.grey.darken3,
+                    secondary: colors.amber.darken3,
+                    info: colors.purple.darken4,
+                    warning: colors.amber.darken4,
+                    error: colors.deepOrange.accent4,
+                    success: colors.lightGreen.base,
+                },
+            },
+        },
+    },
+    /*
+     ** ENV
+     */
+    env: {
+        demoMode: Boolean(process.env.DEMO),
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {},
+    },
 }
