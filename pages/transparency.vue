b5d9db86b8fcb3487cb7ed476e9acf0c1b549363
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-04-12 13:03:09 +0200

    Add GeoMap to Transparency

--- pages/transparency.vue
+++ pages/transparency.vue
@@ -1,28 +1,67 @@
 <template>
   <v-container>
     <v-row>
+      <v-col cols="12" sm="10" offset-sm="1" md="8" offset-md="2">
+        <h1 class="text-center">A friendly crew, nothing to hide!</h1>
+        <p class="text-center">
+          Good privacy starts with trust. Therefore, we want to be as
+          transparent as possible about our finance and how we handle your data.
+        </p>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col>
-        <div class="text-center">
-          <v-icon size="100px">mdi-anchor</v-icon>
-        </div>
         <v-card>
-          <v-card-title class="headline">
-            {{ this.$t('Transparency') }}
+          <v-card-title class="text-center">
+            Finance
           </v-card-title>
           <v-card-text>
-            <p>&hellip;</p>
+            <v-row>
+              <v-col cols="12" sm="6" lg="4" offset-lg="1">
+                <h3>Costs</h3>
+                <div>
+                  <canvas ref="chart-costs" class="chart"></canvas>
+                </div>
+                <ul class="mt-3">
+                  <li>
+                    <b>Infra:</b> Servers, domain names, &hellip; the geeky
+                    stuff
+                  </li>
+                </ul>
+              </v-col>
+
+              <v-col cols="12" sm="6" lg="4" offset-lg="1">
+                <h3>Incomes</h3>
+                <div>
+                  <canvas ref="chart-incomes" class="chart"></canvas>
+                </div>
+                <ul class="mt-3">
+                  <li>
+                    <b>Personal contribution:</b> If the costs are higher than
+                    the incomes, Harbor contributors are donating the
+                    difference.
+                  </li>
+                </ul>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="12" sm="6">
         <v-card>
           <v-card-title class="text-center">
-            Costs vs income
+            Your data is stored in
           </v-card-title>
           <v-card-text>
-            <canvas ref="chart-cost-income" class="chart"></canvas>
+            <v-row>
+              <v-col>
+                <div>
+                  <canvas ref="chart-data-map" class="chart"></canvas>
+                </div>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
@@ -34,46 +73,46 @@
 /* eslint no-unused-vars: "off" */
 
 import Chart from 'chart.js'
+import { topojson } from 'chartjs-chart-geo'
+import europe from '../assets/europe.json'
+const Color = require('color')
 
 export default {
   auth: false,
 
   mounted() {
-    const costsAndIncomes = [
+    const costs = [
       {
         date: new Date(2020, 2, 3), // 03-03-2020
         amount: 5.93,
-        type: 'Cost',
         category: 'Infra',
       },
       {
+        date: new Date(2020, 3, 3), // 03-04-2020
+        amount: 5.93,
+        category: 'Infra',
+      },
+    ]
+
+    const incomes = [
+      {
         date: new Date(2020, 2, 3), // 03-03-2020
         amount: 5.93,
-        type: 'Income',
         category: 'Personal contribution',
       },
       {
         date: new Date(2020, 3, 3), // 03-04-2020
         amount: 5.93,
-        type: 'Cost',
-        category: 'Infra',
-      },
-      {
-        date: new Date(2020, 3, 3), // 03-04-2020
-        amount: 5.93,
-        type: 'Income',
         category: 'Personal contribution',
       },
     ]
 
-    const chartCostIncome = new Chart(this.$refs['chart-cost-income'], {
+    const chartSettings = {
       type: 'bar',
-      data: this.convertToChartData(costsAndIncomes),
       options: {
         maintainAspectRatio: false,
         tooltips: {
-          mode: 'index',
-          intersect: false,
+          mode: 'x',
         },
         scales: {
           xAxes: [
@@ -88,13 +127,98 @@ export default {
           ],
         },
       },
+    }
+
+    const chartCosts = new Chart(this.$refs['chart-costs'], {
+      data: this.convertToChartData(costs, '#D32F2FCC'),
+      ...chartSettings,
     })
-    chartCostIncome.canvas.parentNode.style.height = '400px'
-    chartCostIncome.canvas.parentNode.style.width = '100%'
+    chartCosts.canvas.parentNode.style.height = '400px'
+
+    const chartIncomes = new Chart(this.$refs['chart-incomes'], {
+      data: this.convertToChartData(incomes, '#8BC34ACC'),
+      ...chartSettings,
+    })
+    chartIncomes.canvas.parentNode.style.height = '400px'
+
+    // Geo Map
+    const features = topojson.feature(
+      europe,
+      europe.objects.continent_Europe_subunits
+    ).features
+
+    // Reduce countries to "Zoom" map
+    const limitedEurope = features.filter(
+      (c) => !['Svalbard', 'Russia'].includes(c.properties.geounit)
+    )
+
+    const dataPercentage = {
+      Germany: 100,
+    }
+
+    const colors = limitedEurope.map((c) => {
+      // Get value
+      const value = dataPercentage[c.properties.geounit]
+
+      // Country has no value => Return default
+      if (typeof value === 'undefined') {
+        return 'rgba(255, 255, 255, 0.7)'
+      }
+
+      // Generate color
+      return Color('#64B5F6')
+        .alpha(0.5)
+        .lighten((100 - value) / 100.0)
+        .rgb()
+        .toString()
+    })
+
+    const chartDataMap = new Chart(this.$refs['chart-data-map'], {
+      type: 'choropleth',
+      data: {
+        labels: limitedEurope.map((d) => {
+          const geoUnit = d.properties.geounit
+          let value = dataPercentage[geoUnit]
+          if (typeof value === 'undefined') value = 0
+          return `${geoUnit}: ${value}%`
+        }),
+        datasets: [
+          {
+            label: 'Data location',
+            outline: limitedEurope,
+            backgroundColor: colors,
+            data: limitedEurope.map((d) => ({
+              feature: d,
+              value:
+                typeof dataPercentage[d.properties.geounit] !== 'undefined'
+                  ? dataPercentage[d.properties.geounit]
+                  : 0,
+            })),
+          },
+        ],
+      },
+      options: {
+        showOutline: true,
+        showGraticule: false,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        scale: {
+          projection: 'mercator',
+        },
+        geo: {
+          colorScale: {
+            display: false,
+          },
+        },
+      },
+    })
+    chartDataMap.canvas.parentNode.style.height = '400px'
   },
 
   methods: {
-    convertToChartData(data) {
+    convertToChartData(data, color) {
       // Init vars
       const labels = data.map((item) => this.formatDate(item.date))
       const chartData = {
@@ -105,22 +229,16 @@ export default {
       for (const item of data) {
         const labelIndex = chartData.labels.indexOf(this.formatDate(item.date))
         const dataIndex = chartData.datasets.findIndex(
-          (set) => set.label === item.category && set.stack === item.type
+          (set) => set.label === item.category
         )
 
         if (dataIndex < 0) {
           // Dataset not yet defined
-          let color = ''
-          if (item.type === 'Income') {
-            color = 'rgba(149,221,133,0.5)'
-          } else {
-            color = 'rgba(239,53,16,0.5)'
-          }
-
           const dataset = {
             label: item.category,
-            stack: item.type,
             backgroundColor: color,
+            // hoverBackgroundColor: color,
+            maxBarThickness: 50,
             data: Array(chartData.labels.length).fill(0),
           }
 
